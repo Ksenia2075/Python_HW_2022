8cdacd10450f63f38b1f2cd55fe04109b3005adf
KseniaAk <ksu909090@yandex.ru>
2023-01-30 18:11:11 +0300

    homework6

---
+++ homework6/task6_5.py
@@ -0,0 +1,28 @@
+# ** 5. Реализовать функцию, возвращающую n шуток, сформированных из трех случайных слов,
+# взятых из трёх списков (по одному из каждого)
+# nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+# adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью", "когда-то", "где-то"]
+# adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+
+from random import randrange
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью", "когда-то", "где-то"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+
+def get_jokes(count: int) -> list:
+    """Возвращает список шуток в количестве count"""
+    list_out = []
+    for x in range(count):
+        idx = randrange(len(nouns))
+        ix = randrange(len(adverbs))
+        dx = randrange(len(adjectives))
+        list_out.append(f'{nouns[idx]} {adverbs[ix]} {adjectives[dx]}')
+    return list_out
+    return list_out
+
+joke = get_jokes(3)
+print(joke)
+
